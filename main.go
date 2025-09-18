5be02bdca68bfa3e3188ca6e7b5e8b7c5ee1ac14
bitterfq <amndp64@gmail.com>
2025-09-17 18:33:13 -0700

    Update main function: adjust supplier generation count and add parts generation output

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"github.com/bitterfq/data-ingestion-go/parts"
 	"github.com/bitterfq/data-ingestion-go/suppliers"
 )
 
 func main() {
-	s := suppliers.GenerateSuppliers("tenant_acme", 5000)
+	s := suppliers.GenerateSuppliers("tenant_acme", 10)
+	p := parts.GenerateParts(10)
 	//fmt.Println("%+v\n", s)
 
 	out := suppliers.SupplierWriter("data/suppliers.csv", s)
+	out2 := parts.PartWriter("data/parts.csv", p)
+
+	//fmt.Println("%+v\n", p)
 	print(out)
+	print(out2)
 }
